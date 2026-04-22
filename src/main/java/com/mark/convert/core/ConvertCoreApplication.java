5eab327a891bb442eee4759fcb31d5e45da700ba
Mark Krasnokutsky <mark.krasnokutskiy@osinit.com>
2026-04-22 17:16:59 +0300

    Initial commit

---
+++ src/main/java/com/mark/convert/core/ConvertCoreApplication.java
@@ -0,0 +1,13 @@
+package com.mark.convert.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvertCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConvertCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mark/convert/core/ConvertCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mark.convert.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvertCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
