05332c56d283d22710291b1d117cc94e297e692b
Mark Krasnokutsky <mark.krasnokutskiy@osinit.com>
2026-04-29 20:15:58 +0300

    Запустил сервис как клиент дискавери

--- src/main/java/com/mark/convert/core/ConvertCoreApplication.java
+++ src/main/java/com/mark/convert/core/ConvertCoreApplication.java
@@ -2,11 +2,12 @@ package com.mark.convert.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "com.mark.convert.core")
 @EnableScheduling
+@EnableDiscoveryClient
 public class ConvertCoreApplication {
 
     public static void main(String[] args) {
